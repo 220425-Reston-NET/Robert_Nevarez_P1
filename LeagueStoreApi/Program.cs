09bd3d41806281259fb28d05bf56a5dd01e35170
RobertNevarez <lilgosu999@gmail.com>
2022-06-02 11:58:15 -0400

    Added YML

--- LeagueStoreApi/Program.cs
+++ LeagueStoreApi/Program.cs
@@ -13,13 +13,13 @@ builder.Services.AddSwaggerGen();
 
 // builder.Services.AddScoped<IRepository<OrderHistory>, SQLOrderHistoryRepo>(repo => new SQLOrderHistoryRepo(builder.Configuration.GetConnectionString("Robert_Nevarez_Champion")));
 // builder.Services.AddScoped<IOrderHistoryBL, OrderHistoryBL>();
-builder.Services.AddScoped<IRepository<ChampionInfo>, SQLChampRepo>(repo => new SQLChampRepo(builder.Configuration.GetConnectionString("Robert_Nevarez_Champion")));
+builder.Services.AddScoped<IRepository<ChampionInfo>, SQLChampRepo>(repo => new SQLChampRepo(Environment.GetEnvironmentVariable("Connection_String")));
 builder.Services.AddScoped<IRPBL, SearchRPBL>();
-builder.Services.AddScoped<IRepository<ChampionInfoInventory>, SQLStoreChampion>(repo => new SQLStoreChampion(builder.Configuration.GetConnectionString("Robert_Nevarez_Champion")));
+builder.Services.AddScoped<IRepository<ChampionInfoInventory>, SQLStoreChampion>(repo => new SQLStoreChampion(Environment.GetEnvironmentVariable("Connection_String")));
 builder.Services.AddScoped<IStoreItemsBL, StoreItemsBL>();
-builder.Services.AddScoped<IRepository<ChampionInfo>, SQLChampRepo>(repo => new SQLChampRepo(builder.Configuration.GetConnectionString("Robert_Nevarez_Champion")));
+builder.Services.AddScoped<IRepository<ChampionInfo>, SQLChampRepo>(repo => new SQLChampRepo(Environment.GetEnvironmentVariable("Connection_String")));
 builder.Services.AddScoped<ILeagueStoreBL<ChampionInfo>, SearchChampionBL>();
-builder.Services.AddScoped<IRepository<SummonerInfo>, SQLSumRepo>(repo => new SQLSumRepo(builder.Configuration.GetConnectionString("Robert_Nevarez_Champion")));
+builder.Services.AddScoped<IRepository<SummonerInfo>, SQLSumRepo>(repo => new SQLSumRepo(Environment.GetEnvironmentVariable("Connection_String")));
 builder.Services.AddScoped<ILeagueStoreBL<SummonerInfo>, SearchSummonerBL>();
 
 var app = builder.Build();
